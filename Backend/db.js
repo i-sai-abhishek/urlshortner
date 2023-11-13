e65de80dc401c1f850bf12464f1109feed8f6f0b
saiabhishek-neu <itta.s@northeastern.edu>
2023-11-12 23:17:35 -0500

    Backend Test with mongo link change

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 //cont uri ='mongodb+srv://abishekitta:urlmongo@cluster0.5i29hlk.mongodb.net/'
 const uri = 'mongodb+srv://abishekitta:urlmongo@cluster0.5i29hlk.mongodb.net/test?ssl=true';
 mongoose.connect(uri, {
@@ -15,3 +15,33 @@ db.once('open', () => {
 });
 
 module.exports = db;
+
+*/
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://abishekitta:urlmongo@cluster0.5i29hlk.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
